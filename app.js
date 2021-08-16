5fd9710442ace90f90ba0da7b35aa49fe4f6cb72
Dejan Cetojevic <dejan92ceta@live.com>
2021-08-16 19:47:51 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const accordion = document.getElementsByClassName("accordion-content");
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    console.log(1)
+  })
+}
+
